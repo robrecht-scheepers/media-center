9c1dc3ca96b5bc8c1f77d628a09cf51b6a84ab4d
scheepers <r.scheepers@gmail.com>
2017-05-14 15:23:38 +0200

    progress on repository

--- MediaCenter/MediaCenter/Repository/Repository.cs
+++ MediaCenter/MediaCenter/Repository/Repository.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Windows.Media;
 using MediaCenter.Media;
 
 namespace MediaCenter.Repository
 {
     class Repository
     {
-        
+        private const string ImageFileExtension = "jpg";
+        private const int datePropertyID = 36867;
+
         private string _localStore;
         private string _remoteStore;
         private DateTime _lastSyncFromRemote;
@@ -31,12 +37,55 @@ namespace MediaCenter.Repository
                     content = await reader.ReadToEndAsync();
                 }
                 if(_mediaItems.All(i => i.Name != content))
-                    _mediaItems.Add(new Image {Name = content});
+                    _mediaItems.Add(new ImageItem {Name = content});
             }
         }
 
-        
-        public void AddMediaItems(IEnumerable<MediaItem> newItems)
+        public async Task AddMediaItems(IEnumerable<FileInfo> newItems)
+        {
+            foreach (var fileInfo in newItems.Where(f => f.Extension == ImageFileExtension))
+            {
+                using (FileStream fileStream = fileInfo.OpenRead())
+                {
+                    byte[] buffer = new byte[fileStream.Length];
+                    int numBytesToRead = buffer.Length;
+                    int numBytesRead = 0;
+                    while (numBytesToRead > 0)
+                    {
+                        int n = await fileStream.ReadAsync(buffer, numBytesRead, numBytesToRead);
+                        if (n == 0)
+                            break;
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+
+                    using (Stream destination = new MemoryStream(buffer))
+                    {
+                        var image = new Bitmap(destination);
+
+                        PropertyItem[] propertyItems = image.PropertyItems;
+                        var dateProperty = propertyItems.FirstOrDefault(p => p.Id == datePropertyID);
+                        if (dateProperty != null)
+                            date = encoding.GetString(dateProperty.Value);
+                    }
+                }
+
+                // read image date tag
+                string date = await ReadImageDate(fileInfo);
+
+                // create name for media item
+                string name = CreateItemName(date);
+
+                // create thumbnail 
+
+                // create repository entry
+
+                // 
+            }
+        }
+
+
+        public async Task AddMediaItems(IEnumerable<MediaItem> newItems)
         {
             foreach (var newItem in newItems)
             {
@@ -44,6 +93,7 @@ namespace MediaCenter.Repository
                     continue;
                 _mediaItems.Add(newItem);
             }
+            await Save();
         }
 
         private async Task Save()
@@ -60,6 +110,51 @@ namespace MediaCenter.Repository
         private async Task UpdateLocalStore()
         {
             throw new NotImplementedException();
+        }
+
+        private async Task<string> ReadImageDate(FileInfo fileInfo)
+        {
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string date = "";
+
+            using (FileStream fileStream = fileInfo.OpenRead())
+            {
+                byte[] buffer = new byte[fileStream.Length];
+                int numBytesToRead = buffer.Length;
+                int numBytesRead = 0;
+                while (numBytesToRead > 0)
+                {
+                    int n = await fileStream.ReadAsync(buffer, numBytesRead, numBytesToRead);
+                    if (n == 0)
+                        break;
+                    numBytesRead += n;
+                    numBytesToRead -= n;
+                }
+
+                using (Stream destination = new MemoryStream(buffer))
+                {
+                    var image = new Bitmap(destination);
+                    PropertyItem[] propertyItems = image.PropertyItems;
+                    var dateProperty = propertyItems.FirstOrDefault(p => p.Id == datePropertyID);
+                    if (dateProperty != null)
+                        date = encoding.GetString(dateProperty.Value);
+                }
+            }
+            return date;
+        }
+
+        private string CreateItemName(string date)
+        {
+            var name = date.Replace(":", "").Replace(" ", "");
+            if (_mediaItems.Any(i => i.Name == name))
+            {
+                var originalName = name;
+                int cnt = 2;
+                while (_mediaItems.Any(i => i.Name == name))
+                {
+                    name = originalName + "_" + cnt;
+                }
+            }
         }
     }
 }
