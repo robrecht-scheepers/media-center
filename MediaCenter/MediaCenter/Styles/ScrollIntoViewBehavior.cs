8da10a0e1873fb9fcda559b20471fcf1c97dca70
scheepers <r.scheepers@gmail.com>
2017-06-09 01:44:43 +0200

    Implemented autoscrolling for query result

---
+++ MediaCenter/MediaCenter/Styles/ScrollIntoViewBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace MediaCenter.Styles
+{
+    public sealed class ScrollIntoViewBehavior:Behavior<ListBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.SelectionChanged += ScrollIntoView;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.SelectionChanged -= ScrollIntoView;
+            base.OnDetaching();
+        }
+
+        private void ScrollIntoView(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listBox = (ListBox) sender;
+            if(listBox?.SelectedItem == null)
+                return;
+
+            var item = (ListBoxItem) listBox.ItemContainerGenerator.ContainerFromItem(listBox.SelectedItem);
+            item?.BringIntoView();
+        }
+    }
+}
