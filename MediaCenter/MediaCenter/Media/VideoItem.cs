0857c2943c3da1ae0f11cf12d474e42f89949986
Robrecht Scheepers <Scheepers@kruss.lan>
2017-09-13 09:11:41 +0200

    cleanup of media namespace, data template and empty user control for video item

--- MediaCenter/MediaCenter/Media/VideoItem.cs
+++ MediaCenter/MediaCenter/Media/VideoItem.cs
@@ -8,9 +8,17 @@ namespace MediaCenter.Media
 {
     public class VideoItem : MediaItem
     {
+        private byte[] _firstFrameImage;
+
         public VideoItem(string name) : base(name, MediaType.Video)
         {
 
+        }
+
+        public byte[] FirstFrameImage
+        {
+            get => _firstFrameImage;
+            set => SetValue(ref _firstFrameImage, value);
         }
     }
 }
