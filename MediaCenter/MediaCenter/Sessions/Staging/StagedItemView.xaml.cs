bf775ea165da0b1c8a3c8b1fb213188f5c4b8cfa
Robrecht Scheepers <Scheepers@kruss.lan>
2017-09-25 10:42:10 +0200

    Only show staging item delete button on mouse hover

---
+++ MediaCenter/MediaCenter/Sessions/Staging/StagedItemView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MediaCenter.Sessions.Staging
+{
+    /// <summary>
+    /// Interaction logic for StagedItemView.xaml
+    /// </summary>
+    public partial class StagedItemView : UserControl
+    {
+        public StagedItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
