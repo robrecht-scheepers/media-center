55d8cb8d707722816f9040e4d088d87f130869f1
Robrecht Scheepers <Scheepers@kruss.lan>
2017-10-10 01:36:33 +0200

    Optimisation of prefetching

--- MediaCenter/MediaCenter/Sessions/Query/QuerySession.cs
+++ MediaCenter/MediaCenter/Sessions/Query/QuerySession.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,7 +13,7 @@ namespace MediaCenter.Sessions.Query
 {
     public class QuerySession : SessionBase
     {
-        private readonly int _numberOfPrefetchItems = 3;
+        private readonly int PrefetchBufferSize = int.Parse(ConfigurationManager.AppSettings["PrefetchBufferSize"]);
 
         public QuerySession(IRepository repository) : base(repository)
         {
@@ -49,23 +50,26 @@ namespace MediaCenter.Sessions.Query
             }
         }
 
-        public async Task ContentRequested(string name)
+        public async Task LoadFullImage(string name)
         {
             // decide other prefetch items
             var prefetchList = new List<string>();
             var index = QueryResult.IndexOf(QueryResult.First(x => x.Name == name));
             if (index < QueryResult.Count - 1)
             {
-                for (int i = 1; i < _numberOfPrefetchItems && index + i < QueryResult.Count; i++)
+                for (int i = 1; i < PrefetchBufferSize && index + i < QueryResult.Count; i++)
                 {
-                    prefetchList.Add(QueryResult[index + i].Name);
+                    if(QueryResult[index + i].MediaType == MediaType.Image)
+                        prefetchList.Add(QueryResult[index + i].Name);
                 }
             }
-            if (index > 0)
+            if (index > 0 && QueryResult[index - 1].MediaType == MediaType.Image)
+            {
                 prefetchList.Add(QueryResult[index - 1].Name);
+            }
             var item = QueryResult.First(x => x.Name == name);
-
-            item.Content = await Repository.GetFullImage(name, prefetchList);
+            if(item.MediaType == MediaType.Image)
+                item.Content = await Repository.GetFullImage(name, prefetchList);
         }
         
 
