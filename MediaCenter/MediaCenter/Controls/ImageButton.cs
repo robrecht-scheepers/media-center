b7f7712057e2066659ebefaa6e8a36a8b7eddaca
Robrecht Scheepers <Scheepers@kruss.lan>
2017-11-05 23:54:02 +0100

    added custom buttons

---
+++ MediaCenter/MediaCenter/Controls/ImageButton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MediaCenter.Controls
+{
+    public class ImageButton : Button
+    {static ImageButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageButton),
+                new FrameworkPropertyMetadata(typeof(ImageButton)));
+        }
+
+        public string ImageSource
+        {
+            get { return (string)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(string), typeof(ImageButton), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender, ImageSourceChanged));
+
+
+        private static void ImageSourceChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Application.GetResourceStream(new Uri("pack://application:,,," + (string)e.NewValue));
+        }
+    }
+}
---
+++ MediaCenter/MediaCenter/Controls/ImageToggleButton.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace MediaCenter.Controls
+{
+    public class ImageToggleButton : ToggleButton
+    {
+        static ImageToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ImageToggleButton), new FrameworkPropertyMetadata(typeof(ImageToggleButton)));
+        }
+
+        public string CheckedImage
+        {
+            get { return (string)GetValue(CheckedImageProperty); }
+            set { SetValue(CheckedImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CheckedImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CheckedImageProperty =
+            DependencyProperty.Register("CheckedImage", typeof(string), typeof(ImageToggleButton), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender, ImageSourceChanged));
+
+        public string UncheckedImage
+        {
+            get { return (string)GetValue(UncheckedImageProperty); }
+            set { SetValue(UncheckedImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for UncheckedImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty UncheckedImageProperty =
+            DependencyProperty.Register("UncheckedImage", typeof(string), typeof(ImageToggleButton), new FrameworkPropertyMetadata("", FrameworkPropertyMetadataOptions.AffectsRender, ImageSourceChanged));
+
+
+        private static void ImageSourceChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            Application.GetResourceStream(new Uri("pack://application:,,," + (string)e.NewValue));
+        }
+    }
+}
