bb316a6a374c56487d3821db3fc51130b7eaf2e5
Robrecht Scheepers <Scheepers@kruss.lan>
2017-11-17 00:50:06 +0100

    New control for column separator

---
+++ MediaCenter/MediaCenter/WPF/Controls/ColumnToggleButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MediaCenter.WPF.Controls
+{
+    public class ColumnToggleButton : ImageToggleButton
+    {
+        static ColumnToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ColumnToggleButton), new FrameworkPropertyMetadata(typeof(ColumnToggleButton)));
+        }
+    }
+}
