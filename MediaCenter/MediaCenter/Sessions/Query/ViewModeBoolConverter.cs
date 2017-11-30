871cb761b5dd213737dddc0a6fd16d7494cfedf3
Robrecht Scheepers <Scheepers@kruss.lan>
2017-11-30 23:42:21 +0100

    Buttons for view mode selection

---
+++ MediaCenter/MediaCenter/Sessions/Query/ViewModeBoolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Forms;
+
+namespace MediaCenter.Sessions.Query
+{
+    public class ViewModeBoolConverter : IValueConverter
+    {
+        public ViewModeBoolConverter()
+        {
+            Invert = false;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var mode = (QuerySessionViewModel.ViewMode) value;
+            return Invert ? mode == QuerySessionViewModel.ViewMode.List : mode == QuerySessionViewModel.ViewMode.Detail;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var input = (bool) value;
+
+            if(Invert)
+                return input ? QuerySessionViewModel.ViewMode.List : QuerySessionViewModel.ViewMode.Detail;
+
+            return input ? QuerySessionViewModel.ViewMode.Detail : QuerySessionViewModel.ViewMode.List;
+        }
+
+        public bool Invert { get; set; }
+    }
+}
