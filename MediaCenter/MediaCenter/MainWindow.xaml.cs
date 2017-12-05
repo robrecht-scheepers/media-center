c225084d1579e1759ed2b1aeda94acb3d12b2904
Robrecht Scheepers <Scheepers@kruss.lan>
2017-12-05 13:33:53 +0100

    First version of integrated title bar

--- MediaCenter/MediaCenter/MainWindow.xaml.cs
+++ MediaCenter/MediaCenter/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace MediaCenter
 {
@@ -10,6 +11,50 @@ namespace MediaCenter
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Adjust_OnClick(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+        }
+
+        private void Minimize_OnClick(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Adjusts the WindowSize to correct parameters when Maximize button is clicked
+        /// </summary>
+        private void AdjustWindowSize()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+
+        }
+
+        private void TitleBar_OnMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                if (e.ClickCount == 2)
+                {
+                    AdjustWindowSize();
+                }
+                else
+                {
+                    Application.Current.MainWindow.DragMove();
+                }
         }
     }
 }
