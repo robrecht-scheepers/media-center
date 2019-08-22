17d0c4a1318a0c62ade5ec4cef09e98452d4e0e8
robrecht scheepers <r.scheepers@kruss.de>
2019-08-22 10:55:59 +0200

    MOved view mode button to button bar + fixed issue with three state toggle button

---
+++ MediaCenter/MediaCenter/Sessions/Query/ViewModeToBoolConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MediaCenter.Sessions.Query
+{
+    public class ViewModeToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is ViewMode viewMode))
+                return null;
+
+            switch (viewMode)
+            {
+                case ViewMode.Grid:
+                    return false;
+                case ViewMode.Detail:
+                    return true;
+                case ViewMode.SlideShow:
+                    return null;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return ViewMode.SlideShow;
+
+            var isChecked = (bool?) value;
+
+            return isChecked.Value ? ViewMode.Detail : ViewMode.Grid;
+        }
+    }
+}
