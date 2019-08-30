e7dd51e4165b8ce3e0fae4be0ffd4992635a5c0a
robrecht scheepers <r.scheepers@kruss.de>
2019-08-30 12:23:54 +0200

    Create interface structure for two types of repository

---
+++ MediaCenter/MediaCenter/Repository/IBaseRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediaCenter.Media;
+using MediaCenter.Sessions.Filters;
+
+namespace MediaCenter.Repository
+{
+    public interface IBaseRepository
+    {
+        event EventHandler CollectionChanged;
+        event EventHandler StatusChanged;
+
+        List<string> Tags { get; }
+        Task Initialize();
+        Task<List<MediaItem>> GetQueryItems(IEnumerable<Filter> filters);
+        Task<int> GetQueryCount(IEnumerable<Filter> filters);
+        Task<byte[]> GetThumbnail(MediaItem item);
+        Task<byte[]> GetFullImage(MediaItem item, IEnumerable<MediaItem> prefetch = null);
+        Uri GetContentUri(MediaItem item);
+        System.Uri Location { get; }
+        Task SaveContentToFile(MediaItem item, string filePath);
+        Task SaveMultipleContentToFolder(List<MediaItem> items, string folderPath);
+        string StatusMessage { get; }
+    }
+}
---
+++ MediaCenter/MediaCenter/Repository/ICacheRepository.cs
@@ -0,0 +1,10 @@
+﻿using MediaCenter.Media;
+
+namespace MediaCenter.Repository
+{
+    public interface ICacheRepository : IBaseRepository
+    {
+        void AddToCache(MediaItem item);
+        void RemoveFromCache(MediaItem item);
+    }
+}
